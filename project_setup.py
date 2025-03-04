47ee58f3924354afe6f2f785cff840c9dc89bd8f
Yacouby <yacouby@iguazio.com>
2025-03-04 14:33:43 +0200

    ..

--- project_setup.py
+++ project_setup.py
@@ -13,29 +13,16 @@
 # limitations under the License.
 #
 
-import os
-
 import mlrun
 
 
 def setup(project: mlrun.projects.MlrunProject) -> mlrun.projects.MlrunProject:
     source = project.get_param("source")
-    secrets_file = project.get_param("secrets_file")
-    default_image = project.get_param("default_image")
 
     # Set project git/archive source and enable pulling latest code at runtime
     if source:
         print(f"Project Source: {source}")
         project.set_source(project.get_param("source"), pull_at_runtime=True)
-
-    # Create project secrets and also load secrets in local environment
-    if secrets_file and os.path.exists(secrets_file):
-        project.set_secrets(file_path=secrets_file)
-        mlrun.set_env_from_file(secrets_file)
-
-    # Set default project docker image - functions that do not specify image will use this
-    if default_image:
-        project.set_default_image(default_image)
 
     # MLRun Functions - note that paths are relative to the project context (./src)
     project.set_function(
@@ -46,7 +33,7 @@ def setup(project: mlrun.projects.MlrunProject) -> mlrun.projects.MlrunProject:
     )
 
     project.log_artifact("data", target_path="https://s3.wasabisys.com/iguazio/data/iris/iris.data.raw.csv")
-    
+
     # Save and return the project:
     project.save()
     return project
